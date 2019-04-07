cb1fa8db9c8479d4873ae379e9e616994050b40f
Megan Weijiang <meganweijiang@utexas.edu>
2019-04-07 17:21:33 -0500

    fix styling for small screen sizes

--- client/src/components/SubmitButton.js
+++ client/src/components/SubmitButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SubmitButton = (props) => (
-  <div class="submit-button clearfix">
+  <div className="submit-button clearfix">
     { !props.validateEmail(props.email) && props.email !== '' && <div className="invalid">Please enter a valid email address.</div> }
     <button onClick={props.handleSubmit} disabled={!props.validateEmail(props.email)}>Submit</button>
   </div>
